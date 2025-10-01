7c9cbe1550879719f7925fda94400bcaad1a7d3b
YoungsikYoon <ysyoon97@postech.ac.kr>
2025-10-01 08:43:58 +0000

    Deploying to gh-pages from @ YoungsikYoon/YoungsikYoon.github.io@cd1b234d1e5bc75afcf00285ecde446e0c04c79b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "YoungsikYoon/ysyoon97.github.io",
+    "data-repo": "YoungsikYoon/YoungsikYoon.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
